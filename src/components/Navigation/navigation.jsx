a11b6875aad6e0f4b91e677a122aa2f27a7e7b9c
Stefan30 <secrieru23022@gmail.com>
2024-09-13 12:35:29 +0300

    Try to fix nav opening bug

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,10 +1,12 @@
 import './navigation.css'
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import darkSVG from "../../../public/svgs/darkSVG.svg";
 import sunSVG from "../../../public/svgs/sunSVG.svg";
+import cDownSVG from "../../../public/svgs/caret-down.svg";
 import {useSelector} from "react-redux";
 import {getProject} from "../../../utils/store/utils-store/utils-store-selectors.js";
 const Navigation = ({night, setNight, page, setPage, scrollListener}) => {
+    const project = useSelector(getProject)
     const menuItems = [
         'Presentation', 'Education', 'Experience', 'Projects', 'Skills', 'Legal'
     ]
@@ -27,6 +29,11 @@ const Navigation = ({night, setNight, page, setPage, scrollListener}) => {
         // document.querySelector('.info-page').classList.remove('slide-left')
         // document.querySelector('.info-page').classList.add('slide-left2')
     }
+
+    const closeNav = () => {
+        document.querySelector('.navigation ul').classList.add('hidden')
+        document.querySelector('.menu-controls').classList.add('hidden')
+    }
     // console.log('bbbb', project)
     return (
         <div className={`navigation ${night ? 'nav-dark' : ''}`}>
@@ -38,6 +45,7 @@ const Navigation = ({night, setNight, page, setPage, scrollListener}) => {
                 <li><img className='scheme-option' src={!night ? darkSVG : sunSVG} onClick={() => goDark()}/></li>
             </ul>
             <h1 className='page-title'>{menuItems[page]} Island</h1>
+            {project !== null && <img src={cDownSVG} className='menu-controls' onClick={closeNav} />}
         </div>
     )
 }
